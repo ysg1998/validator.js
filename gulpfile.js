cbe667edf941da7fda0d3ce583374b3d3be91ec2
jaywcjlove <398188662@qq.com>
2016-03-10 23:41:15 +0800

    添加压缩任务；更新README.md

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,21 @@
 var gulp = require("gulp");
 var rename = require("gulp-rename");
+var uglify = require('gulp-uglify');
+var gzip = require('gulp-gzip')
+var gutil = require('gulp-util');
 var umd = require("gulp-umd");
 var watch = require("gulp-watch");
 
 
-gulp.task('default',['script']);
+gulp.task('default',['build']);
 gulp.task('watch',function(){
-    gulp.watch('src/**/*',['script'])
+    gulp.watch('src/**/*',['build'])
 })
 
 
-gulp.task('script', function () {
+gulp.task('build', function () {
+
+    // 压缩成 validator.min.js
     gulp.src('src/*.js')
         .pipe(umd({
             exports: function(file) {
@@ -20,6 +25,29 @@ gulp.task('script', function () {
                 return 'Validator';
             }
         }))
+        .pipe(uglify({
+            mangle: true,
+            output:{
+                comments:false
+            }
+        }).on('error',gutil.log))
         .pipe(rename({suffix:".min"})) 
         .pipe(gulp.dest('./dist'));
+
+    // 生成 validator.js
+    gulp.src('src/*.js')
+        .on('error',gutil.log)
+        .pipe(umd({
+            dependencies: function(file) {
+                return ['Validator'];
+            },
+            exports: function(file) {
+                return 'Validator';
+            },
+            namespace: function(file) {
+                return 'Validator';
+            }
+        }))
+        .pipe(gulp.dest('./dist/'));
+
 });
