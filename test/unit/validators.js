b0758dcec7d6edd64fb867aac1d29bc122094f4e
jaywcjlove <398188662@qq.com>
2016-03-16 15:50:46 +0800

    添加测试用例

--- test/unit/validators.js
+++ test/unit/validators.js
@@ -5,6 +5,17 @@ var v =  new Validator();
 
 describe("validators", function() {
 
+    it("required() 必填验证", function() {
+        expect( v.required('') ).to.be.false; 
+        expect( v.required('t') ).to.be.true;
+    });
+    
+    it("isUrl() URL 验证", function() {
+        expect( v.isUrl('://www.ss') ).to.be.false; 
+        expect( v.isUrl('http://baidu.com') ).to.be.true;
+        expect( v.isUrl('hps://www.baidu.com') ).to.be.true;
+    });
+
     it("isEmail() 邮箱验证", function() {
         expect( v.isEmail('d.s.s.d@qq.com.cn') ).to.be.true; 
         expect( v.isEmail('d.s-s.d@qq.com.cn') ).to.be.true; 
@@ -40,10 +51,29 @@ describe("validators", function() {
         expect( v.isIp('192.168.1324.123') ).to.be.false;
     });
 
+
+    // * 13段：130、131、132、133、134、135、136、137、138、139
+    // * 14段：145、147
+    // * 15段：150、151、152、153、155、156、157、158、159
+    // * 17段：170、176、177、178
+    // * 18段：180、181、182、183、184、185、186、187、188、189
+    // * 国际码 如：中国(+86)
     it("isPhone() 手机号码验证", function() {
-        expect( v.isPhone('13611779473') ).to.be.true; 
-        expect( v.isPhone('+8613611779473') ).to.be.true; 
-        expect( v.isPhone('+23613611779473') ).to.be.true; 
+        expect( v.isPhone('13688889890') ).to.be.true; 
+        expect( v.isPhone('13012341233') ).to.be.true; 
+        expect( v.isPhone('+8613688889890') ).to.be.true; 
+        expect( v.isPhone('+23613688889890') ).to.be.true; 
+        expect( v.isPhone('19088889890') ).to.be.false; 
+    });
+
+    // 国家代码(2到3位)-区号(2到3位)-电话号码(7到8位)-分机号(3位)
+    it("isTel() 座机号码验证", function() {
+        expect( v.isTel('086-021-4433432-233') ).to.be.true; 
+        expect( v.isTel('+086-021-4433432-233') ).to.be.true; 
+        expect( v.isTel('+086-021-4433432-23') ).to.be.false; 
+        expect( v.isTel('+086-021-4433432-2333') ).to.be.true; 
+        expect( v.isTel('+086-021-4433432-1') ).to.be.false; 
+        expect( v.isTel('13012341233') ).to.be.false; 
     });
 
 });
