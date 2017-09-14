b0d46b51b6e378065a7882e72aa4fabea57ebb49
Matt Ma <dayed.hell@gmail.com>
2017-09-14 09:57:34 +0800

    Update validator.js
    增加一些常用规则与函数

--- src/validator.js
+++ src/validator.js
@@ -40,7 +40,11 @@ var regexs = {
     /**
      * @descrition 匹配 URL
      */
-    url:/[a-zA-z]+:\/\/[^\s]/
+    url:/[a-zA-z]+:\/\/[^\s]/,
+    money: /^(0|[1-9]\d*)(\.\d+)?$/,
+    english: /^[A-Za-z]+$/,
+    chinese: /^[\u0391-\uFFE5]+$/,
+    percent: /^(?:[1-9][0-9]?|100)(?:\.[0-9]{1,2})?$/,
 }
 
 var _testHook = {
@@ -56,6 +60,10 @@ var _testHook = {
     is_phone:function(field){return regexs.phone.test( backVal(field) );},
     // 验证URL
     is_url:function(field){return regexs.url.test( backVal(field) );},
+    is_money: function (field) {return regexs.money.test(backVal(field));},
+    is_english: function (field) {return regexs.english.test(backVal(field));},
+    is_chinese: function (field) {return regexs.chinese.test(backVal(field));},
+    is_percent: function (field) {return regexs.percent.test(backVal(field));},
     // 是否为必填
     required: function(field) {
         var value =  backVal(field) ;
@@ -80,6 +88,22 @@ var _testHook = {
         var value2 = backVal(this.fields[newField].element);
 
         return value1 == value2;
+    },
+    // 拒绝与某个字段相等,比如登录密码与交易密码情况
+    different: function (field, newField) {
+        return !this.same(field, newField)
+    },
+    // 直接判断字符串是否相等
+    contains: function (field, value) {
+        var value1 = backVal(field);
+
+        return value1 == value;
+    },
+    // 用于服务条款,是否同意时相当有用,不限制checkbox与radio,有可能submit button直接附带value情况
+    accepted: function (field) {
+        var value = backVal(field);
+
+        return 'YES' == value.toUpperCase() || 'ON' == value.toUpperCase() || 1 == value || false == value ? true : false;
     }
 }
 
